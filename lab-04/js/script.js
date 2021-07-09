e5727de929e14f3f4921239a0b3f0e47c9a5ac9b
IsaacIpenza <benjamin.ipre@gmail.com>
2021-07-09 18:49:17 -0500

    Implementation for the Clear/Mark/Delete Button

--- lab-04/js/script.js
+++ lab-04/js/script.js
@@ -14,21 +14,55 @@ function TodoPost(e){
     var to_do = document.getElementById("todoText").value;
     var list = document.getElementById("todoList");
 
-    var currentListHTML = list.innerHTML;
-    list.innerHTML = currentListHTML + `<input type = "checkbox" name="todo" /> ${to_do} <br>`;
+    // var currentListHTML = list.innerHTML;
+    // list.innerHTML = currentListHTML + `<input type = "checkbox" name="todo" /> ${to_do} <br>`;
+
+
+    var div = document.createElement("div");
+    var checkbox = document.createElement("input");
+    var label = document.createElement("label");
+
+    checkbox.type = "checkbox";
+    checkbox.name = "todo";
+    label.textContent = to_do;
+    
+    div.appendChild(checkbox);
+    div.appendChild(label);
+    list.appendChild(div);
 
     document.getElementById("todoText").value = "";
     to_do ="";
 }
 
 function TodoClear(e){
+    var todos = document.getElementsByName("todo");
+    // console.log(todos);
 
+    todos.forEach(element => {
+        element.checked = false;
+    })
 }
 
 function TodoMark(e){
+    var todos = document.getElementsByName("todo");
+    // console.log(todos);
 
+    todos.forEach(element => {
+        element.checked = true;
+    })
 }
 
 function TodoDel(e){
+    // var list = document.getElementsById("todoList");
+    // list.innerHTML = "";
+
+    var todos = document.getElementsByName("todo");
+
+    for (let i = todos.length - 1; i >= 0; i--) {
+
+        if(todos[i].checked){
+            todos[i].parentNode.remove();
+        }
+    }
 
 }
