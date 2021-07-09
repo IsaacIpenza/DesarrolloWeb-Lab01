8890dfa54eddb6804bdb61d16751b8d14cb4a7ca
IsaacIpenza <benjamin.ipre@gmail.com>
2021-07-08 19:09:37 -0500

    Implementation of the Post Button functionality
    -We do it in the class 08/07/2021 colaborating with the teacher

---
+++ lab-04/js/script.js
@@ -0,0 +1,34 @@
+var post = document.getElementById("ButtonPost");
+var clear = document.getElementById("ButtonClear");
+var mark = document.getElementById("ButtonMark");
+var del = document.getElementById("ButtonDelete");
+
+post.addEventListener("click",TodoPost);
+clear.addEventListener("click",TodoClear);
+mark.addEventListener("click",TodoMark);
+del.addEventListener("click",TodoDel);
+
+function TodoPost(e){
+    e.preventDefault();
+
+    var to_do = document.getElementById("todoText").value;
+    var list = document.getElementById("todoList");
+
+    var currentListHTML = list.innerHTML;
+    list.innerHTML = currentListHTML + `<input type = "checkbox" name="todo" /> ${to_do} <br>`;
+
+    document.getElementById("todoText").value = "";
+    to_do ="";
+}
+
+function TodoClear(e){
+
+}
+
+function TodoMark(e){
+
+}
+
+function TodoDel(e){
+
+}
